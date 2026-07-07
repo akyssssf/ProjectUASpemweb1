0ae143f68d33089a1864db4d748af1d1b7e8d827
akyas <b00124354@gmail.com>
2026-07-08 01:05:59 +0700

    Use seeded SQLite database by default

--- config/database.php
+++ config/database.php
@@ -35,7 +35,7 @@ return [
         'sqlite' => [
             'driver' => 'sqlite',
             'url' => env('DB_URL'),
-            'database' => env('DB_DATABASE', database_path('database.sqlite')),
+            'database' => env('DB_DATABASE') ?: base_path('db_klinik'),
             'prefix' => '',
             'foreign_key_constraints' => env('DB_FOREIGN_KEYS', true),
             'busy_timeout' => null,
